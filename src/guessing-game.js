b0929f840f90df8b701fab401a376a05b3d635be
RYZHIK-PC\Ryzhik <bredvmoiyshi@gmail.com>
2017-09-29 18:45:08 +0300

    possible solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,40 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.middle = 0;
+        this.low = 0;
+        this.gre = 0;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        let res = 0;
+        res = Math.floor((this.min + this.max) / 2);
+        this.middle = res;
+        return res;
     }
 
     lower() {
 
+        let res = 0;
+        this.max = this.middle + 1;
+        res = Math.floor((this.min + this.max) / 2);
+        this.middle = res;
+        return res;
     }
 
     greater() {
 
+        let res = 0;
+        this.min = this.middle;
+        res = Math.floor((this.min + this.max) / 2);
+        this.middle = res + 1;
+        return res;
     }
 }
 
